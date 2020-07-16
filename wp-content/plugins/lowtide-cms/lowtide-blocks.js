d44cdec1319ebdcffcf651cd5302c43b91f5901d
Seong <sryoo6@gatech.edu>
2020-07-15 21:47:46 -0400

    Remove extraneous files

--- wp-content/plugins/lowtide-cms/lowtide-blocks.js
+++
@@ -1,4 +0,0 @@
-var el = wp.element.createElement;
-var InnerBlocks = wp.blockEditor.InnerBlocks;
-var MediaUpload = wp.blockEditor.MediaUpload;
-var MediaUploadCheck = wp.blockEditor.MediaUploadCheck;
